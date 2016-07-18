34efef3645168f1d5c65e7a8c29c4a8c924146e9
Michal Kalman <michalkalman@gmail.com>
2016-07-18 19:49:01 +0200

    fix: table function js

--- src/inventory/views/event_views.py
+++ src/inventory/views/event_views.py
@@ -11,76 +11,77 @@ from vanilla import CreateView, DeleteView, ListView, UpdateView
 from inventory.service import InventoryService
 
 import logging
+
 logger = logging.getLogger(__name__)
 service = InventoryService()
+
 
 # Views for groups management
 
 
 class EventView(LoginRequiredMixin):
-	def process_form(self, request, form):
-		items = []
-		for field in request.POST:
-			if field.startswith('p_'):
-				id = request.POST[field]
-				p = Product.objects.get(pk=id)
-				items.append(p)
+    def process_form(self, request, form):
+        items = []
+        for field in request.POST:
+            if field.startswith('p_'):
+                id = request.POST[field]
+                p = Product.objects.get(pk=id)
+                items.append(p)
 
-		if form.is_valid():
-			self.object = form.save()
-			self.object.products = []
-			self.object.products.add(*items)
-			self.object.save()
-			return True
-		return False
+        if form.is_valid():
+            self.object = form.save()
+            self.object.products = []
+            self.object.products.add(*items)
+            self.object.save()
+            return True
+        return False
 
-	def prepare_form(self, context):
-		context['products'] = service.get_all_products_in_cart()
-		if hasattr(self, 'object'):
-			context['items'] = self.object.products
-		return context
+    def prepare_form(self, context):
+        context['products'] = service.get_all_products_in_cart()
+        if hasattr(self, 'object'):
+            context['items'] = self.object.products
+        return context
 
 
 class ListEvents(LoginRequiredMixin, ListView):
-	model = Event
-	queryset = Event.objects.all()
+    model = Event
+    queryset = Event.objects.all()
 
 
 class CreateEvent(EventView, CreateView):
-	model = Event
-	form_class = EventForm
-	success_url = reverse_lazy('list_events')
+    model = Event
+    form_class = EventForm
+    success_url = reverse_lazy('list_events')
 
-	def get_context_data(self, **kwargs):
-		return self.prepare_form(super(CreateEvent, self).get_context_data(**kwargs))
+    def get_context_data(self, **kwargs):
+        return self.prepare_form(super(CreateEvent, self).get_context_data(**kwargs))
 
-	def post(self, request, *args, **kwargs):
-			form = self.get_form(data=request.POST)
-			if self.process_form(request, form):
-				return HttpResponseRedirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        form = self.get_form(data=request.POST)
+        if self.process_form(request, form):
+            return HttpResponseRedirect(self.get_success_url())
 
-			return self.form_invalid(form)
+        return self.form_invalid(form)
 
 
 class EditEvent(EventView, UpdateView):
-	model = Event
-	form_class = EventForm
-	success_url = reverse_lazy('list_events')
+    model = Event
+    form_class = EventForm
+    success_url = reverse_lazy('list_events')
 
-	def get_context_data(self, **kwargs):
-		return self.prepare_form(super(EditEvent, self).get_context_data(**kwargs))
+    def get_context_data(self, **kwargs):
+        return self.prepare_form(super(EditEvent, self).get_context_data(**kwargs))
 
-	def post(self, request, *args, **kwargs):
-		self.object = self.get_object()
-		form = self.get_form(data=request.POST, instance=self.object)
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form(data=request.POST, instance=self.object)
 
-		if self.process_form(request, form):
-			return HttpResponseRedirect(self.get_success_url())
+        if self.process_form(request, form):
+            return HttpResponseRedirect(self.get_success_url())
 
-		return self.form_invalid(form)
+        return self.form_invalid(form)
 
 
 class DeleteEvent(LoginRequiredMixin, DeleteView):
-	model = Event
-	success_url = reverse_lazy('list_events')
-
+    model = Event
+    success_url = reverse_lazy('list_events')
