bd78a572126f80308885228e8dab7fee368a742e
Michal Kalman <michalkalman@gmail.com>
2016-07-19 09:33:22 +0200

    update importing

--- src/sync/serializers.py
+++ src/sync/serializers.py
@@ -7,14 +7,21 @@ from django.utils.six import BytesIO
 from inventory.models import Product
 from rest_framework_xml.parsers import XMLParser
 
+#import logging
+
+#logger = logging.getLogger(__name__)
 
 class ProductSerializer(serializers.ModelSerializer):
     """
     Serializing all the Product
     """
+    code = serializers.CharField(max_length=20)
+    origin_name = serializers.CharField(max_length=60)
+    price = serializers.FloatField()
+    amount = serializers.IntegerField()
     class Meta:
         model = Product
-        fields = ('code', 'origin_name', 'price')
+    #    fields = ('code', 'origin_name', 'price', 'amount')
 
 
 class ProductDeserializer:
@@ -23,7 +30,8 @@ class ProductDeserializer:
     def parse(content):
         stream = BytesIO(content)
         data = XMLParser().parse(stream)
-        print(data)
+        #logger.debug('parsing data: %s', data)
+        print('parsing data: %s', data)
         return data
 
 
--- src/sync/service.py
+++ src/sync/service.py
@@ -3,27 +3,48 @@ Service for synchronization
 """
 from inventory.utils import file_to_blob
 from sync.serializers import ProductDeserializer, ProductSerializer
+from inventory.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
+#import logging
+
+#logger = logging.getLogger(__name__)
 
 def sync_products_from_file(input_file):
 	try:
 		blob = file_to_blob(input_file)
+		#logger.debug(input_file)
+		print(input_file)
 	except IOError:
+		#logger.error('cannot open file', input_file)
 		print('cannot open file', input_file)
 
 	else:
 		counter = 0
 		data = ProductDeserializer.parse(blob)
+		print(data)
 		if data:
 			for row in data:
 				serializer = ProductSerializer(data=row)
 				if serializer.is_valid():
-					serializer.validated_data
-					serializer.save()
+					#print('test %s', serializer['code'])
+					code = serializer.validated_data['code']
+					try:
+						product = Product.objects.get(code=code)
+						print('update product')
+						product.price = serializer.validated_data['price']
+						product.amount = serializer.validated_data['amount']
+						product.save()
+					except ObjectDoesNotExist:
+						print('import product', row)
+						serializer.save()
+
 					counter += 1
-					print('save product', row)
+					#logger.debug('import product', row)
+					print('import product', row)
 
 				else:
-					print('not valid parsing data', row)
+					#logger.error('not valid parsing data %s', row)
+					print('not valid parsing data %s', row)
 
 		return counter
